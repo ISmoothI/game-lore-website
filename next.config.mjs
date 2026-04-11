f861f548c2e649c85018d82b3780f568c8cbcd51
Jonathan A. <106954634+ISmoothI@users.noreply.github.com>
2026-04-11 01:47:31 -0700

    ~ fixed assetPrefix else statement in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,9 @@ const isProd = process.env.NODE_ENV === "production";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
-    output: "export",
-    basePath: isProd ? "/game-lore-website" : ``,
-    assetPrefix: isProd ? "/game-lore-website/" : "/"
+    output: 'export',
+    basePath: isProd ? '/game-lore-website' : '',
+    assetPrefix: isProd ? '/game-lore-website/' : ''
 };
 
 export default nextConfig;
