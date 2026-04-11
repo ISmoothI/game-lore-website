b963ec985a3ef64eac0cd3bc5c49f36707634a20
Jonathan A. <106954634+ISmoothI@users.noreply.github.com>
2026-04-11 01:21:24 -0700

    +~ added path changes in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+    basePath: isProd ? "/game-lore-website" : ``,
+    assetsPath: isProd ? "/game-lore-website/" : "/"
 };
 
 export default nextConfig;
