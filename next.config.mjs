037949e4a258d48c269af0ec59ff1369a4deecd4
Jonathan A. <106954634+ISmoothI@users.noreply.github.com>
2026-04-11 02:13:18 -0700

    + added image unoptimization setting

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,10 @@ const nextConfig = {
   /* config options here */
     output: 'export',
     basePath: isProd ? '/game-lore-website' : '',
-    assetPrefix: isProd ? '/game-lore-website/' : ''
+    assetPrefix: isProd ? '/game-lore-website/' : '',
+    images: {
+        unoptimized: true
+    }
 };
 
 export default nextConfig;
