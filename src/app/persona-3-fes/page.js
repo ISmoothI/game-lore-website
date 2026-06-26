4d229ad1aa37dc1e678068885363abebb159633a
Jonathan A. <106954634+ISmoothI@users.noreply.github.com>
2026-06-25 23:42:31 -0700

    + added icons for conditions + added image of main character and starter persona + added input functionality for dates + added health and spirit bar changes when changing the values ~ adjusted sizes of images depending on display

--- src/app/persona-3-fes/page.js
+++ src/app/persona-3-fes/page.js
@@ -4,7 +4,13 @@ import Image from "next/image";
 
 import {Inter} from "next/font/google";
 import {useState} from "react";
-import statusImage from "./assets/images/placeholderimage.svg";
+
+import greatIcon from "./assets/images/icon_great.svg";
+import goodIcon from "./assets/images/icon_good.svg";
+import tiredIcon from "./assets/images/icon_tired.svg";
+import sickIcon from "./assets/images/icon_sick.svg";
+import orpheusImage from "./assets/images/persona_orpheus.svg";
+import makotoImage from "./assets/images/character_makoto.svg";
 
 import styles from './page.module.css';
 
@@ -14,52 +20,72 @@ const inter = Inter({
 });
 
 export default function MainMenu() {
-    const timeOptions = ["Daytime", "Afternoon", "Evening", "Nighttime", "Dark Hour"];
+    const timeOptions = ["Before Dawn", "Early Morning", "Morning", "Daytime", "Lunchtime", "Afternoon", "After School", "Evening", "Nighttime", "Late Night", "Dark Hour"];
     const dayOptions = ["M", "T", "W", "Th", "F", "Sa", "Su"];
     const menuOptions = {
-        SKILL: "Use/view skills.",
-        ITEM: "Use/view items.",
-        PERSONA: "",
-        EQUIP: "",
-        STATUS: "",
-        "S.LINK": "",
-        SYSTEM: "",
+        "SKILL": "Use/view skills.",
+        "ITEM": "Use/view items.",
+        "PERSONA": "Change/view Personas.",
+        "EQUIP": "Change/view equipment.",
+        "STATUS": "View detailed status",
+        "S.LINK": "View Social Link details.",
+        "SYSTEM": "Info/Config.",
     };
-    const feelingOptions = ["Great", "Good", "Tired", "Sick"];
+    const conditionOptions = {
+        Great: greatIcon,
+        Good: goodIcon,
+        Tired: tiredIcon,
+        Sick: sickIcon
+    };
     const personaOptions = {
-        Orpheus: "Fool",
-        Thanatos: "Death",
+        Orpheus: {
+            arcana: "Fool",
+            src: orpheusImage
+        },
+        // Thanatos: "Death",
     };
+    const totalConditionAmount = Object.entries(conditionOptions).length;
+    const totalPersonaAmount = Object.entries(personaOptions).length;
 
     const [currTimeIndex, setCurrTimeIndex] = useState(0);
     const [currDayIndex, setCurrDayIndex] = useState(0);
-    const [currFeelingIndex, setCurrFeelingIndex] = useState(0);
+    const [currHealth, setCurrHealth] = useState(18);
+    const [maxHealth, setMaxHealth] = useState(18);
+    const [currSpirit, setCurrSpirit] = useState(18);
+    const [maxSpirit, setMaxSpirit] = useState(18);
+    const [currConditionIndex, setCurrConditionIndex] = useState(0);
     const [currPersonaIndex, setCurrPersonaIndex] = useState(0);
     const [hoveredOption, setHoveredOption] = useState(null);
+
+    const percentageCalc = (currVal, maxVal) => {
+        let maxNum = Math.max(0, (maxVal > 0 ? (currVal / maxVal) * 100 : 0));
+
+        return Math.min(94, maxNum);
+    }
 
     return (
         <>
             <div className={`${styles.page} ${inter.className}`}>
                 <div className={styles.main}>
                     <div className={styles.content}>
-                        {/*<Image className={styles.character__image} src={statusImage} alt={"Makoto Yuki image"} width={400} height={400} />*/}
+                        <Image className={styles.persona__image} src={Object.values(personaOptions)[currPersonaIndex].src} alt={`${Object.keys(personaOptions)[currPersonaIndex]} image icon`} />
+                        <Image className={styles.character__image} src={makotoImage} alt={"Makoto Yuki image"} />
                         <div className={styles.menu__textcontainer}>
                             <div className={styles.menu__textcontent}>
                                 <h1 className={styles.menu__text}>MAIN</h1>
                                 <h1 className={styles.menu__text} aria-hidden={true}>MAIN</h1>
                             </div>
                         </div>
-                        {/*<Image className={styles.persona__image} src={statusImage} alt={`${Object.keys(personaOptions)[currPersonaIndex]} image icon`} width={400} height={400} />*/}
                         <div className={styles.header}>
-                            <h2>COMMAND</h2>
+                            <h2 className={styles.command}>COMMAND</h2>
                             <div className={styles.header__time}>
                                 <div className={styles.time} onClick={() => setCurrTimeIndex((currTimeIndex + 1) % timeOptions.length)}>
                                     <h3 className={styles.time__text}>{timeOptions[currTimeIndex]}</h3>
                                 </div>
                                 <div className={styles.header__date}>
-                                    <h3 className={styles.date__number}>8</h3>
-                                    <h3 className={styles.date__day}>/</h3>
-                                    <h3 className={styles.date__number}>22</h3>
+                                    <input className={styles.date__inputone} type={"text"} inputMode={"numeric"} pattern={"[0-9]{2}"} maxLength={2} defaultValue={"1"} size={2} title={"Click to change the month of the date."}/>
+                                    <h3 className={styles.date__divider}>/</h3>
+                                    <input className={styles.date__inputtwo} type={"text"} inputMode={"numeric"} pattern={"[0-9]{2}"} maxLength={2} defaultValue={"12"} size={2} title={"Click to change the day of the date."}/>
                                     <div className={styles.dot}/>
                                     <h3 className={styles.date__day} onClick={() => setCurrDayIndex((currDayIndex + 1) % dayOptions.length)}>{dayOptions[currDayIndex]}</h3>
                                 </div>
@@ -82,33 +108,33 @@ export default function MainMenu() {
                                         <h4>PLv</h4>
                                         <input className={styles.level__input} type={"text"} inputMode={"numeric"} pattern={"[0-9]{2}"} maxLength={2} defaultValue={10} size={2} title={"Click to change the level of the character."}/>
                                     </div>
-                                    <h3 style={{fontStyle: "initial", fontWeight: "500"}}>Makoto Yuki</h3>
+                                    <h3 className={styles.character__name}>Makoto Yuki</h3>
                                     <div className={styles.levelname__underline}/>
                                 </div>
                                 <div className={styles.health}>
-                                    <h4 style={{boxSizing: "border-box", padding: "0 4px"}}>HP</h4>
-                                    <input className={styles.point__leftinput} type={"text"} inputMode={"numeric"} pattern={"[0-9]{3}"} maxLength={3} defaultValue={18} size={3} title={"Click to change the health points of the character."}/>
+                                    <h4 className={styles.status__title}>HP</h4>
+                                    <input className={styles.point__leftinput} type={"text"} inputMode={"numeric"} pattern={"[0-9]{3}"} maxLength={3} defaultValue={currHealth} onChange={(e) => setCurrHealth(e.target.value)} size={3} title={"Click to change the health points of the character."}/>
                                     <h4>/</h4>
-                                    <input className={styles.point__rightinput} type={"text"} inputMode={"numeric"} pattern={"[0-9]{3}"} maxLength={3} defaultValue={18} size={3} title={"Click to change the health points of the character."}/>
+                                    <input className={styles.point__rightinput} type={"text"} inputMode={"numeric"} pattern={"[0-9]{3}"} maxLength={3} defaultValue={maxHealth} onChange={(e) => setMaxHealth(e.target.value)} size={3} title={"Click to change the health points of the character."}/>
                                     <div className={styles.pointcontainer__base}>
-                                        <div className={styles.healthbar__fill} />
+                                        <div style={{width: `${percentageCalc(currHealth, maxHealth)}%`}} className={styles.healthbar__fill} />
                                     </div>
                                     <div className={styles.health__underline}/>
                                 </div>
                                 <div className={styles.spirit}>
-                                    <h4 style={{boxSizing: "border-box", padding: "0 4px"}}>SP</h4>
-                                    <input className={styles.point__leftinput} type={"text"} inputMode={"numeric"} pattern={"[0-9]{3}"} maxLength={3} defaultValue={18} size={3} title={"Click to change the spirit points of the character."}/>
+                                    <h4 className={styles.status__title}>SP</h4>
+                                    <input className={styles.point__leftinput} type={"text"} inputMode={"numeric"} pattern={"[0-9]{3}"} maxLength={3} defaultValue={18} onChange={(e) => setCurrSpirit(e.target.value)} size={3} title={"Click to change the spirit points of the character."}/>
                                     <h4>/</h4>
-                                    <input className={styles.point__rightinput} type={"text"} inputMode={"numeric"} pattern={"[0-9]{3}"} maxLength={3} defaultValue={18} size={3} title={"Click to change the spirit points of the character."}/>
+                                    <input className={styles.point__rightinput} type={"text"} inputMode={"numeric"} pattern={"[0-9]{3}"} maxLength={3} defaultValue={18} onChange={(e) => setMaxSpirit(e.target.value)} size={3} title={"Click to change the spirit points of the character."}/>
                                     <div className={styles.pointcontainer__base}>
-                                        <div className={styles.spirit__fill} />
+                                        <div style={{width: `${percentageCalc(currSpirit, maxSpirit)}%`}} className={styles.spirit__fill} />
                                     </div>
                                     <div className={styles.spirit__underline}/>
                                 </div>
                             </div>
-                            <div className={styles.feeling} onClick={() => setCurrFeelingIndex((currFeelingIndex + 1) % feelingOptions.length)}>
-                                <h3>{feelingOptions[currFeelingIndex]}</h3>
-                                <Image className={styles.feeling__image} src={statusImage} alt={`${feelingOptions[currFeelingIndex]} image icon`} />
+                            <div className={styles.condition} onClick={() => setCurrConditionIndex((currConditionIndex + 1) % totalConditionAmount)}>
+                                <h3 className={styles.condition__text}>{Object.keys(conditionOptions)[currConditionIndex]}</h3>
+                                <Image className={styles.condition__image} src={Object.values(conditionOptions)[currConditionIndex]} alt={`${conditionOptions[currConditionIndex]} image icon`} />
                             </div>
                             <div className={styles.persona}>
                                 <div className={styles.levelname}>
@@ -116,11 +142,11 @@ export default function MainMenu() {
                                         <h4>Lv</h4>
                                         <input className={styles.level__input} type={"text"} inputMode={"numeric"} pattern={"[0-9]{2}"} maxLength={2} defaultValue={10} size={2} title={"Click to change the level of the persona."}/>
                                     </div>
-                                    <h3>{Object.keys(personaOptions)[currPersonaIndex]}</h3>
+                                    <h3 className={styles.persona__name} onClick={() => setCurrPersonaIndex((currPersonaIndex + 1) % totalPersonaAmount)}>{Object.keys(personaOptions)[currPersonaIndex]}</h3>
                                 </div>
                                 <div className={styles.arcana}>
                                     <div className={styles.circle}/>
-                                    <h3>{Object.values(personaOptions)[currPersonaIndex]}</h3>
+                                    <h3>{Object.values(personaOptions)[currPersonaIndex].arcana}</h3>
                                     <div className={styles.circle}/>
                                 </div>
                                 <div className={styles.persona__underline}/>
@@ -138,7 +164,7 @@ export default function MainMenu() {
                             </div>
                             <div className={styles.menuoption__description}>
                                 <div className={styles.menuoption__point}/>
-                                <h3>{menuOptions[hoveredOption] || "."}</h3>
+                                <h3>{menuOptions[hoveredOption]}&nbsp;</h3>
                             </div>
                         </div>
                     </div>
