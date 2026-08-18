39f3d83d54ec5b19fc661896956d82e391c81155
Jonathan A. <106954634+ISmoothI@users.noreply.github.com>
2026-08-17 23:32:52 -0700

    + added main font + added gaps between menu options + added header + added css styles for unique text + added sections for 3 main party members and sub party members + added guests sections

--- src/app/xenoblade-chronicles-definitive-edition/page.js
+++ src/app/xenoblade-chronicles-definitive-edition/page.js
@@ -1,6 +1,16 @@
 'use client'
 
+import Image from "next/image";
+
+import {Archivo} from "next/font/google";
+
 import styles from './page.module.css';
+
+const archivo = Archivo({
+    subsets: ["latin"],
+    // style: "normal",
+    weight: ["100"]
+});
 
 export default function MainMenu() {
     const menuOptions = {
@@ -15,30 +25,40 @@ export default function MainMenu() {
         "Collectables": "",
     };
 
+    const characters = [
+            {"id": 1, "name": "Shulk", "skills": ["", "",""]},
+            {"id": 2, "name": "Reyn", "skills": ["", "",""]},
+            {"id": 3, "name": "Fiora", "skills": ["", "",""]},
+            {"id": 4, "name": "Sharla", "skills": ["", "",""]},
+            {"id": 5, "name": "Dunban", "skills": ["", "",""]},
+            {"id": 6, "name": "Riki", "skills": ["", "",""]},
+            {"id": 7, "name": "Melia", "skills": ["", "",""]},
+    ];
+
     return (
         <>
-            <div className={`${styles.page}`}>
+            <div className={`${styles.page} ${archivo.className}`}>
                 <div className={styles.main}>
                     <div className={styles.header}>
                         <div className={styles.section__text}>
-
+                            <h1>Main Menu</h1>
                         </div>
                         <div className={styles.data}>
-                            <div className={styles.data__gold}>
+                            <div className={styles.data__container}>
                                 <div className={styles.data__goldimg}>
-
+                                    <Image src={"/images/image.svg"} alt={"Placeholder"} width={16} height={16} />
                                 </div>
                                 <div className={styles.data__goldnums}>
-
+                                    <h3>000000000</h3>
                                 </div>
                             </div>
 
-                            <div className={styles.data__time}>
+                            <div className={styles.data__container}>
                                 <div className={styles.data__timeimg}>
-
+                                    <Image src={"/images/image.svg"} alt={"Placeholder"} width={16} height={16} />
                                 </div>
                                 <div className={styles.data__timenums}>
-
+                                    <h3>000:00</h3>
                                 </div>
                             </div>
                         </div>
@@ -61,14 +81,55 @@ export default function MainMenu() {
                                 })}
                             </div>
                             <div className={styles.systembutton}>
-
+                                <Image src={"/images/image.svg"} alt={"Placeholder"} width={20} height={20} />
+                                <h4>System Menu</h4>
                             </div>
                         </div>
                         <div className={styles.party__main}>
-
+                            <div className={styles.party__mainoption}>
+                                <div className={styles.party__mainheader}>
+                                    <Image src={"/images/image.svg"} alt={"Placeholder"} width={30} height={30} />
+                                    <h3 className={styles.leader}>Leader</h3>
+                                </div>
+                                <Image src={"/images/image.svg"} alt={"Placeholder"} width={100} height={100} />
+                                <div className={styles.party__maintext}>
+                                    <h4 className={styles.skill}>Skill</h4>
+                                    <h1>Name</h1>
+                                    <div className={styles.level}>
+                                        <h3 className={styles.party__label}>Lv</h3>
+                                        <h3>1</h3>
+                                    </div>
+                                    <div className={styles.health}>
+                                        <h3 className={styles.party__label}>HP</h3>
+                                        <h3>1</h3>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                         <div className={styles.party__sub}>
-
+                            <div className={styles.party__suboption}>
+                                <Image src={"/images/image.svg"} alt={"Placeholder"} width={100} height={100} />
+                                <div className={styles.party__subtext}>
+                                    <h4 className={styles.skill}>Skill</h4>
+                                    <h1>Name</h1>
+                                    <div className={styles.level}>
+                                        <h3 className={styles.party__label}>Lv</h3>
+                                        <h3>1</h3>
+                                    </div>
+                                    <div className={styles.health}>
+                                        <h3 className={styles.party__label}>HP</h3>
+                                        <h3>1</h3>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={styles.party__guestsection}>
+                                <h3>Guests</h3>
+                                <div className={styles.guests}>
+                                    <div className={styles.guest}>
+                                        <Image src={"/images/image.svg"} alt={"Placeholder"} width={20} height={20} />
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
 
